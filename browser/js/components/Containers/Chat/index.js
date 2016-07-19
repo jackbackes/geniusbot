18972a55304fbc477d91894213abb5d66bc6e171
thejohnbackes <jbackes@thisismotive.com>
2016-07-18 22:40:00 -0400

    add Chat container

--- browser/js/components/Containers/Chat/index.js
+++ browser/js/components/Containers/Chat/index.js
@@ -2,11 +2,38 @@
 
 import React from 'react';
 import {ChatBox} from './imports';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+
+const style={
+  chat: {
+    minHeight: '100vh',
+    minWidth: '500px',
+    width: '30vw'
+  },
+  chatInput: {
+    position: 'fixed',
+    bottom: '0px',
+    width: '100vw',
+    backgroundColor: 'white'
+  }
+}
 
 const Chat = React.createClass({
   render(){
     return (
-      <ChatBox />
+      <div>
+        <Paper
+          children={(
+            <ChatBox />
+          )}
+          zDepth={3}
+          style={style.chat}
+        />
+        <Paper style={style.chatInput} children={
+          <TextField id={'chatInput'} style={style.chatInput} />
+        } />
+      </div>
     )
   }
 })
