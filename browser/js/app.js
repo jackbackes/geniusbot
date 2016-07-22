9daaa21af52e4d4d8cf6e3a53bd11d37ff25ef63
thejohnbackes <jbackes@thisismotive.com>
2016-07-22 14:03:07 -0400

    refactors app.js

--- browser/js/app.js
+++ browser/js/app.js
@@ -10,11 +10,15 @@ import { syncHistoryWithStore } from 'react-router-redux'
 import routes from './components/Providers/App/routes'
 import configureStore from './components/Providers/App/configureStore'
 import injectTapEventPlugin from 'react-tap-event-plugin'
+// import {GeniusBotObserver, GeniusBotSelector} from './components/Providers/ChatProvider/chatModules';
+
 
 injectTapEventPlugin()
 
-const store = configureStore(browserHistory)
+export const store = configureStore(browserHistory)
 const history = syncHistoryWithStore(browserHistory, store)
+
+// GeniusBotObserver(store, GeniusBotSelector);
 
 render(
   <Provider store={store}>
