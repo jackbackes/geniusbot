c46bb609e8db537ce4ae276cb6a30da64e96f0c2
thejohnbackes <jbackes@thisismotive.com>
2016-07-25 10:56:20 -0400

    fixed problem with es6 proxies. proxies are not implemented in Safari 9. this now works in Safari 9.

--- browser/js/components/Presentationals/ChatBox/index.js
+++ browser/js/components/Presentationals/ChatBox/index.js
@@ -10,18 +10,31 @@ const _ = require('lodash');
 const ChatBox = React.createClass({
   render(){
     let {messages, users, selfId} = this.props;
-    let renderedMessages = messages.map(message => new Proxy(
-      message,
-      {
-        get: (target, prop) => {
-            if(prop === 'itemIndex') {
-              return message.id
-            } else if(prop === 'user') {
-              return users.get(message.userId.toString())
-            } else { return target[prop] }
-          }
-      }
-    ));
+    let renderedMessages = messages.map(message => {
+      Object.defineProperties(message, {
+        "itemIndex": {
+          configurable: true,
+          get: ()=> message.id
+        },
+        "user": {
+          configurable: true,
+          get: ()=> users.get(message.userId.toString())
+        }
+      })
+      return message;
+      // new Proxy(
+      //   message,
+      //   {
+      //     get: (target, prop) => {
+      //         if(prop === 'itemIndex') {
+      //           return message.id
+      //         } else if(prop === 'user') {
+      //           return users.get(message.userId.toString())
+      //         } else { return target[prop] }
+      //       }
+      //   }
+      // )
+    });
     return(
       <List children={
         renderedMessages.map(
